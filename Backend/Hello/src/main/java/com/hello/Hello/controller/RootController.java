fd787a7da08d72ed889cacb0cef4a156b264d410
KamaleshVelmurugan30 <kamaleshvelmurugan30@gmail.com>
2026-01-22 16:50:55 +0530

    Add root controller

---
+++ Backend/Hello/src/main/java/com/hello/Hello/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.hello.Hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "FarmChainX Backend is Running 🚀";
+    }
+}
